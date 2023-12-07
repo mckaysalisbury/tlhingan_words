8a84e2f866133ddf055ad1a0d1d6a087e5eb550f
McKay Salisbury <mckaysalisbury@hotmail.com>
2023-12-07 16:19:11 -0700

    support for vowel patterns

--- tlhIngan.js
+++ tlhIngan.js
@@ -1,3 +1,4 @@
-const vowels = ['a', 'e', 'I', 'o', 'u']
-const consonants = ['b', 'ch', 'D', 'gh', 'H', 'j', 'l', 'm', 'n', 'ng', 'p', 'q', 'Q', 'r', 'S', 't', 'tlh', 'v', 'w', 'y', "'"]
-const extraPieces = ["w'", "y'", 'rgh']
+const vowels = ['a', 'e', 'I', 'o', 'u'];
+const consonants = ['b', 'ch', 'D', 'gh', 'H', 'j', 'l', 'm', 'n', 'ng', 'p', 'q', 'Q', 'r', 'S', 't', 'tlh', 'v', 'w', 'y', "'"];
+const extraPieces = ["w'", "y'", 'rgh'];
+const syllableTerminatingConsonantClusters = [""].concat(consonants, extraPieces);
